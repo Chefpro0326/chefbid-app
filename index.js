eb17912808caeac886c07e32cd6c1c6fce1dceeb
Chefpro0326 <info@chefbidpro.com>
2026-05-07 18:37:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
-// ChefBid PRO v3 - Stripe enabled
+import { cors } from "hono/cors";
 import { readFileSync } from "fs";
+// ChefBid PRO v3 - Stripe enabled
 
 const app = new Hono();
 app.use("/*", cors());
